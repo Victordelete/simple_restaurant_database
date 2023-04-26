7774dc0844cba94901bc8ec1f8d6c70ac35d4f59
Victor <victorh_delete@hotmail.com>
2023-04-26 07:37:49 -0300

    Correção main
    Correção de arquivo principal e correção dos script gerais.

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import mysql.connector
 from mysql.connector import Error
 
-def create_server_connection(host_name, user_name, user_password):
+
+#SETANDO CONEXÃO
+host_name = "localhost"
+user_name = "root"
+user_password = "3141592" #minha senha
+
+
+#ACIONANDO QUERYS DOS ARQUIVOS DO BANCO
+#aciona_query(connection, "teste.sql")
+files = ["TABELAS/1_cria_tabelas_principal.sql",
+        "TABELAS/2_cria_tabelas_endereco.sql",
+        "TABELAS/3_cria_tabelas_contato.sql",
+        "TABELAS/4_cria_tabelas_estoque.sql",
+        "TABELAS/5_cria_tabelas_pedido.sql",
+        "TABELAS/6_cria_tabelas_ordem_compra.sql",
+        "DADOS/1_insere_dados.sql",
+        "DADOS/2_insere_dados.sql",
+        "BUSCAS/1_cria_procedures.sql",
+        "BUSCAS/1_cria_views.sql"]
+
+for file in files:
+
     connection = None
     try:
         connection = mysql.connector.connect(
@@ -9,38 +30,21 @@ def create_server_connection(host_name, user_name, user_password):
             user=user_name,
             passwd=user_password
         )
-        print("MySQL Database connection successful")
+        cursor = connection.cursor()
+        print("MySQL Database connection successful")  
     except Error as err:
         print(f"Error: '{err}'")
-
-    return connection
-
-def aciona_query(connection, query_file):
-    query = open(query_file, 'r').read()
 
     try:
-        cursor = connection.cursor()
-        cursor.execute(query)
-        cursor.close()
+        query = open(file, encoding="utf8").read()
     except Error as err:
         print(f"Error: '{err}'")
 
-#SETANDO CONEXÃO
-host_name = "localhost"
-user_name = "root"
-user_pasword = "3141592" #minha senha
+    try:
+        cursor.nextset()
+        cursor.execute(query)
+        connection.close()
+    except Error as err:
+        print(f"Error: '{err}'")
 
-connection  = create_server_connection(host_name, user_name, user_pasword)
-
-#ACIONANDO QUERYS DOS ARQUIVOS DO BANCO
-aciona_query(connection, "teste.sql")
-aciona_query(connection, "TABELAS/2_cria_tabelas_endereco.sql")
-aciona_query(connection, "TABELAS/3_cria_tabelas_contato.sql")
-aciona_query(connection, "TABELAS/4_cria_tabelas_estoque.sql")
-aciona_query(connection, "TABELAS/5_cria_tabelas_pedido.sql")
-aciona_query(connection, "TABELAS/6_cria_tabelas_ordem_compra.sql")
-
-aciona_query(connection, "DADOS/insere_dados.sql")
-
-connection.commit()
 connection.close() #close connection
